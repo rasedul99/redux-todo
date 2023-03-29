6ee38bae6e2faae134438fe63c3f213c18b7869f
MD Rasedul Islam <rasedul2199@gmail.com>
2023-03-30 00:44:31 +0600

    todo added in todo list

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,32 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 import tickImage from "../images/double-tick.png";
 import noteImage from "../images/notes.png";
+import { added } from "../redux/todos/actions";
 
 export default function Header() {
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch();
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(added(input));
+    setInput("");
+  };
   return (
     <div>
-      <form className="flex items-center bg-gray-100 px-4 py-4 rounded-md">
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center bg-gray-100 px-4 py-4 rounded-md"
+      >
         <img src={noteImage} className="w-6 h-6" alt="Add todo" />
         <input
           type="text"
           placeholder="Type your todo"
+          value={input}
+          onChange={handleInput}
           className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
         />
         <button
