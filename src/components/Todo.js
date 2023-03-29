a322f6614962e6ffc23b913c53b2044cb41dd851
MD Rasedul Islam <rasedul2199@gmail.com>
2023-03-29 22:41:11 +0600

    either linethrough or not in todolist condition applied

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -36,7 +36,11 @@ export default function Todo({ todo }) {
         )}
       </div>
 
-      <div className="select-none flex-1 line-through">{todo?.text}</div>
+      <div
+        className={`select-none flex-1 ${todo?.completed && "line-through"}`}
+      >
+        {todo?.text}
+      </div>
 
       <div
         className={`flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer border-green-500 hover:bg-green-500 ${
