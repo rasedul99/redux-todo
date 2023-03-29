877ff8791a86dbf7fe9e0019918849b3bb24c45c
MD Rasedul Islam <rasedul2199@gmail.com>
2023-03-29 23:58:27 +0600

    filterByStatus and filterByColor made two function for filtering todolist

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,27 +4,28 @@ import Todo from "./Todo";
 export default function TodoList() {
   const todos = useSelector((state) => state.todos);
   const filters = useSelector((state) => state.filters);
-  console.log(filters);
-  console.log(todos);
+  const filterByStatus = (todo) => {
+    switch (filters.status) {
+      case "Complete":
+        return todo.completed;
+      case "Incomplete":
+        return !todo.completed;
+      default:
+        return true;
+    }
+  };
+  const filterByColor = (todo) => {
+    if (filters.colors.length > 0) {
+      return filters.colors.includes(todo?.color);
+    }
+    return true;
+  };
+
   return (
     <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
       {todos
-        .filter((todo) => {
-          switch (filters.status) {
-            case "Complete":
-              return todo.completed;
-            case "Incomplete":
-              return !todo.completed;
-            default:
-              return true;
-          }
-        })
-        .filter((todo) => {
-          if (filters.colors.length > 0) {
-            return filters.colors.includes(todo?.color);
-          }
-          return true;
-        })
+        .filter(filterByStatus)
+        .filter(filterByColor)
         .map((todo) => {
           return <Todo todo={todo} key={todo.id} />;
         })}
