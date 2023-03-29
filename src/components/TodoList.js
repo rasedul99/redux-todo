553214d69e3ff4f46d039b2b5a48b2312aba1e81
MD Rasedul Islam <rasedul2199@gmail.com>
2023-03-29 22:36:37 +0600

    todo displaying depend on filter on footer

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,12 +3,31 @@ import Todo from "./Todo";
 
 export default function TodoList() {
   const todos = useSelector((state) => state.todos);
+  const filters = useSelector((state) => state.filters);
+  console.log(filters);
   console.log(todos);
   return (
     <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-      {todos.map((todo) => {
-        return <Todo todo={todo} key={todo.id} />;
-      })}
+      {todos
+        .filter((todo) => {
+          switch (filters.status) {
+            case "Complete":
+              return todo.completed;
+            case "Incomplete":
+              return !todo.completed;
+            default:
+              return true;
+          }
+        })
+        .filter((todo) => {
+          if (filters.colors.length > 0) {
+            return filters.colors.includes(todo?.color);
+          }
+          return true;
+        })
+        .map((todo) => {
+          return <Todo todo={todo} key={todo.id} />;
+        })}
       <Todo />
     </div>
   );
